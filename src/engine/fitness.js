0d864853458fe343d7c8475f77f66fbcd5571c57
Samy Laumonier <samy.laumonier@gmail.com>
2018-04-04 13:23:25 +0200

    Advanced solution

--- src/engine/fitness.js
+++ src/engine/fitness.js
@@ -1,6 +1,32 @@
 import { engine } from './';
 
 engine.fitness = function (individual) {
-  const score = 0;
-  return score;
+  const { startPoint, endPoint } = this.getConfig();
+
+  let position = {
+    x: startPoint.x,
+    y: startPoint.y,
+  };
+
+  individual.bestDistance = Infinity;
+  individual.totalMoves = 0;
+
+  individual.genes.some(gene => {
+    position = this.moveIndividual(position, gene);
+    individual.totalMoves++;
+
+    const distance = this.getDistance(position, endPoint);
+
+    if (!individual.win) {
+      individual.win = distance === 0;
+    }
+
+    if (distance < individual.bestDistance) {
+      individual.bestDistance = distance;
+    }
+
+    return individual.win;
+  });
+
+  return individual.bestDistance + individual.totalMoves;
 };
