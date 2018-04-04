9b38e8f488ea79a293315f4db25693d9152f430e
Samy Laumonier <samy.laumonier@gmail.com>
2018-04-04 13:20:26 +0200

    Simple solution
    (cherry picked from commit 1dc41e4af2e6ab3244deb13fc8bfc20cbcd9b6aa)

--- src/engine/fitness.js
+++ src/engine/fitness.js
@@ -1,6 +1,27 @@
 import { engine } from './';
 
 engine.fitness = function (individual) {
-  const score = 0;
-  return score;
+  const { startPoint, endPoint } = this.getConfig();
+
+  let position = {
+    x: startPoint.x,
+    y: startPoint.y,
+  };
+
+  let bestDistance = Infinity;
+
+  individual.genes.forEach(gene => {
+    position = this.moveIndividual(position, gene);
+    const distance = this.getDistance(position, endPoint);
+
+    if (distance < bestDistance) {
+      bestDistance = distance;
+    }
+
+    if (distance === 0) {
+      individual.win = true;
+    }
+  });
+
+  return bestDistance;
 };
--- src/engine/mutate.js
+++ src/engine/mutate.js
@@ -1,5 +1,8 @@
 import { engine } from './';
 
 engine.mutate = function (individual) {
+  const i = Math.floor(Math.random() * individual.genes.length);
+  individual.genes[i] = this.getRandomDirection();
+
   return individual;
 };
